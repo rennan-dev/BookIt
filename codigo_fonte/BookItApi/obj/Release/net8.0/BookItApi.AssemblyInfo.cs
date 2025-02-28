c86beea9f3a5ccc165e5c7873e4888839806cae6
Anderson Souza <153129699+arsouza81@users.noreply.github.com>
2025-02-28 15:58:52 -0400

    documentacao

--- codigo_fonte/BookItApi/obj/Release/net8.0/BookItApi.AssemblyInfo.cs
+++ codigo_fonte/BookItApi/obj/Release/net8.0/BookItApi.AssemblyInfo.cs
@@ -12,13 +12,9 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("BookItApi")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< HEAD
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd7e53762104e6604db9114fb351335c9f28ab3b")]
-=======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d65c71b2c52ebce087487c473144dc35c0ce1a73")]
->>>>>>> 7599162 (documentacao)
 [assembly: System.Reflection.AssemblyProductAttribute("BookItApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BookItApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
